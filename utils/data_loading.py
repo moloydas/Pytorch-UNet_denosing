17e3c46ddcf1b2f5eb8ad4b284b3ab4903d778d1
moloydas <moloy.bitspilani@gmail.com>
2024-06-01 09:09:17 -0700

    Update data_loading.py

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -12,11 +12,82 @@ from pathlib import Path
 from torch.utils.data import Dataset
 from tqdm import tqdm
 
+class DenoiserDataset(Dataset):
+    def __init__(self, images_dir: str, mask_dir: str, scale: float = 1.0, mask_suffix: str = ''):
+        self.images_dir = Path(images_dir)
+        self.mask_dir = Path(mask_dir)
+        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
+        self.scale = scale
+        self.mask_suffix = mask_suffix
+
+        self.ids = [splitext(file)[0] for file in listdir(images_dir) if isfile(join(images_dir, file)) and not file.startswith('.')]
+
+        if not self.ids:
+            raise RuntimeError(f'No input file found in {images_dir}, make sure you put your images there')
+
+        logging.info(f'Creating dataset with {len(self.ids)} examples')
+        logging.info('Scanning mask files to determine unique values')
+        with Pool(processes=4) as p:
+            unique = list(tqdm(
+                p.imap(partial(unique_mask_values, mask_dir=self.mask_dir, mask_suffix=self.mask_suffix), self.ids),
+                total=len(self.ids)
+            ))
+
+        # self.mask_values = list(sorted(np.unique(np.concatenate(unique), axis=0).tolist()))
+        # logging.info(f'Unique mask values: {self.mask_values}')
+
+    def __len__(self):
+        return len(self.ids)
+
+    @staticmethod
+    def preprocess(pil_img, scale, is_mask):
+        if is_mask:
+            # mask = np.zeros((newH, newW), dtype=np.int64)
+            # for i, v in enumerate(mask_values):
+            #     if img.ndim == 2:
+            #         mask[img == v] = i
+            #     else:
+            #         mask[(img == v).all(-1)] = i
+
+            return pil_img.transpose((2, 0, 1))
+        else:
+            w, h = pil_img.size
+            newW, newH = int(scale * w), int(scale * h)
+            pil_img = pil_img.resize((newW, newH), resample=Image.BICUBIC if not is_mask else Image.NEAREST)
+            img = np.asarray(pil_img)
+
+            if img.ndim == 2:
+                img = img[np.newaxis, ...]
+            else:
+                img = img.transpose((2, 0, 1))
+
+            if (img > 1).any():
+                img = img / 255.0
+
+            return img
+
+    def __getitem__(self, idx):
+        name = self.ids[idx]
+        mask_file = list(self.mask_dir.glob(name + self.mask_suffix))
+        img_file = list(self.images_dir.glob(name + '.*'))
+
+        assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
+        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
+        mask = load_image(mask_file[0])
+        img = load_image(img_file[0])
+
+        img = self.preprocess(img, 2, is_mask=False)
+        mask = self.preprocess(mask, self.scale, is_mask=True)
+
+        return {
+            'image': torch.as_tensor(img.copy()).float().contiguous(),
+            'mask': torch.as_tensor(mask.copy()).float().contiguous()
+        }
 
 def load_image(filename):
     ext = splitext(filename)[1]
     if ext == '.npy':
-        return Image.fromarray(np.load(filename))
+        return np.load(filename)
     elif ext in ['.pt', '.pth']:
         return Image.fromarray(torch.load(filename).numpy())
     else:
