f5eb88a99b8674dd4a00eb32075ead4ad38ac201
Moloy Das <modas@ucsd.edu>
2024-06-01 09:33:50 -0700

    changes in dataloader

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -95,7 +95,7 @@ def load_image(filename):
 
 
 def unique_mask_values(idx, mask_dir, mask_suffix):
-    mask_file = list(mask_dir.glob(idx + mask_suffix + '.*'))[0]
+    mask_file = list(mask_dir.glob(idx + mask_suffix))[0]
     mask = np.asarray(load_image(mask_file))
     if mask.ndim == 2:
         return np.unique(mask)
